184d8c53f60a024131fa8cb069503e834e2c0d3f
weeklies <80141759+weeklies@users.noreply.github.com>
2023-03-17 11:59:09 +0000

    Do genre pages

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,13 +1,50 @@
 const Genre = require("../models/genre");
+const Book = require("../models/book");
+const async = require("async");
+const mongoose = require("mongoose");
 
 // Display list of all Genre.
-exports.genre_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre list");
+exports.genre_list = function (req, res, next) {
+  Genre.find()
+    .sort([["name", "ascending"]])
+    .exec((err, list_genres) => {
+      if (err) next(err);
+      res.render("genre_list", {
+        title: "Genre List",
+        genre_list: list_genres,
+      });
+    });
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+exports.genre_detail = (req, res, next) => {
+  async.parallel(
+    {
+      genre(callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+
+      genre_books(callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (results.genre == null) {
+        // No results.
+        const err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+      }
+      if (err) return next(err);
+
+      // Successful, so render
+      res.render("genre_detail", {
+        title: "Genre Detail",
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 // Display Genre create form on GET.
