bf819240225990be874ac93db6ecb6a5229e46b7
weeklies <80141759+weeklies@users.noreply.github.com>
2023-03-25 21:41:13 +0000

    Add update for genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -111,7 +111,11 @@ exports.genre_delete_get = (req, res, next) => {
     },
     (err, results) => {
       if (err) return next(err);
-      if (results.genre == null) res.redirect("/catalog/genres");
+      if (results.genre == null) {
+        const err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+      }
 
       res.render("genre_delete", {
         title: "Delete Genre",
@@ -158,11 +162,50 @@ exports.genre_delete_post = (req, res, next) => {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+exports.genre_update_get = (req, res, next) => {
+  Genre.findById(req.params.id).exec((err, genre) => {
+    if (err) return next(err);
+    if (genre == null) {
+      const err = new Error("Genre not found");
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render("genre_form", {
+      title: "Update Genre",
+      genre: genre,
+    });
+  });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-};
+exports.genre_update_post = [
+  // Validate and sanitize the name field.
+  body("name", "Genre name required").trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id, //This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("genre_form", {
+        title: "Create Genre",
+        genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre) => {
+        if (err) return next(err);
+        res.redirect(thegenre.url);
+      });
+    }
+  },
+];
